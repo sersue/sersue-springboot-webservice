e027a7a6b12df49c58211a5edb2d9b74aabbcaf8
MoonSueLym <msr56501354@gmail.com>
2021-02-02 20:18:51 +0900

    혹시 이거..?

--- src/test/java/com/sersue/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/sersue/book/springboot/web/IndexControllerTest.java
@@ -19,6 +19,6 @@ public class IndexControllerTest {
     @Test
     public void 메인페이지_로딩(){
         String body = this.restTemplate.getForObject("/",String.class);
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스 Ver 1.0.1");
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
     }
 }
